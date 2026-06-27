53262e966f8e65ef53a75c297da23970212fa582
Aayush Aryan <aayushar1601@gmail.com>
2026-06-28 01:20:37 +0530

    fixing the same thing again

--- Frontend/app/lib/api/api.ts
+++ Frontend/app/lib/api/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const baseURL = "/api/backend";
-if (!baseURL) throw new Error("NEXT_PUBLIC_API_URL is not defined");
 const api = axios.create({
   baseURL: baseURL,
   headers: {
