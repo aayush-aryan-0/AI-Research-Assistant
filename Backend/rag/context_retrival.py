86d4fcb406921a00e1b9bc73a1b309c535dc42da
Aayush Aryan <aayushar1601@gmail.com>
2026-06-27 19:34:52 +0530

    Refactor context retrieval to use a global model

--- Backend/rag/context_retrival.py
+++ Backend/rag/context_retrival.py
@@ -1,30 +1,33 @@
-from sentence_transformers import SentenceTransformer
 from db.embeddings import get_chunk
 import uuid
 from basemodel import Context
 
+_model = None
+
+def get_model():
+    global _model
+    if _model is None:
+        from sentence_transformers import SentenceTransformer
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model
 
 async def context_retrieval(
-        project_id:uuid.UUID,
-        query:str, 
-        limit:int=3)->str:
-    __model = SentenceTransformer(
-        "all-MiniLM-L6-v2"
-    )
-    query_embedding = __model.encode(query).tolist()
-    chunks:list[Context] = await get_chunk(
+        project_id: uuid.UUID,
+        query: str,
+        limit: int = 3) -> str:
+    
+    model = get_model()
+    query_embedding = model.encode(query).tolist()
+    chunks: list[Context] = await get_chunk(
         project_id=project_id,
         target=query_embedding,
         limit=limit
     )
     return "\n\n".join(
-    f"""
+        f"""
     [Chunk {i+1} | similarity score: {row.similarity:.3f}]:
     [Metadata {row.chunk_metadata}]
     {row.chunk}
-
     """
-    for i, row in enumerate(chunks)
-
+        for i, row in enumerate(chunks)
     )
-   
